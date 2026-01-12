fe1181b38d8b2f6ee4c6118ea2ffd2d19e7ab438
alok kumar kaushal <85283226+alok-kumar8765@users.noreply.github.com>
2026-01-12 16:28:17 +0530

    Create config.py

---
+++ System-Updating-via-py/auto_updater/config.py
@@ -0,0 +1,16 @@
+# ================= EMAIL CONFIG =================
+EMAIL_CONFIG = {
+    "enabled": True,
+    "smtp": "smtp.gmail.com",
+    "port": 587,
+    "email": "your_email@gmail.com",
+    "password": "APP_PASSWORD",  # Gmail App Password
+    "to": "notify@gmail.com"
+}
+
+# ================= TELEGRAM CONFIG =================
+TELEGRAM_CONFIG = {
+    "enabled": True,
+    "bot_token": "BOT_TOKEN",
+    "chat_id": "CHAT_ID"
+}
