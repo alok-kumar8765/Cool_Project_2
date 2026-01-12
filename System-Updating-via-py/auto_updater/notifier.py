0c525fd42b1878edb9aded567ca826e18b341e7f
alok kumar kaushal <85283226+alok-kumar8765@users.noreply.github.com>
2026-01-12 16:20:41 +0530

    Create notifier.py

---
+++ System-Updating-via-py/auto_updater/notifier.py
@@ -0,0 +1,14 @@
+EMAIL_CONFIG = {
+    "enabled": True,
+    "smtp": "smtp.gmail.com",
+    "port": 587,
+    "email": "your_email@gmail.com",
+    "password": "APP_PASSWORD",
+    "to": "notify@gmail.com"
+}
+
+TELEGRAM_CONFIG = {
+    "enabled": True,
+    "bot_token": "BOT_TOKEN",
+    "chat_id": "CHAT_ID"
+}
