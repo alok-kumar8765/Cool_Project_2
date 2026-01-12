040483dee114cb8bfc0fcd018721e34c79405c8b
alok kumar kaushal <85283226+alok-kumar8765@users.noreply.github.com>
2026-01-12 16:21:24 +0530

    Update notifier.py

--- System-Updating-via-py/auto_updater/notifier.py
+++ System-Updating-via-py/auto_updater/notifier.py
@@ -1,14 +1,33 @@
-EMAIL_CONFIG = {
-    "enabled": True,
-    "smtp": "smtp.gmail.com",
-    "port": 587,
-    "email": "your_email@gmail.com",
-    "password": "APP_PASSWORD",
-    "to": "notify@gmail.com"
-}
+import smtplib, requests
+from email.mime.text import MIMEText
 
-TELEGRAM_CONFIG = {
-    "enabled": True,
-    "bot_token": "BOT_TOKEN",
-    "chat_id": "CHAT_ID"
-}
+from config import EMAIL_CONFIG, TELEGRAM_CONFIG
+
+
+def send_email(subject, body):
+    msg = MIMEText(body)
+    msg["Subject"] = subject
+    msg["From"] = EMAIL_CONFIG["email"]
+    msg["To"] = EMAIL_CONFIG["to"]
+
+    server = smtplib.SMTP(EMAIL_CONFIG["smtp"], EMAIL_CONFIG["port"])
+    server.starttls()
+    server.login(EMAIL_CONFIG["email"], EMAIL_CONFIG["password"])
+    server.send_message(msg)
+    server.quit()
+
+
+def send_telegram(text):
+    url = f"https://api.telegram.org/bot{TELEGRAM_CONFIG['bot_token']}/sendMessage"
+    requests.post(url, data={
+        "chat_id": TELEGRAM_CONFIG["chat_id"],
+        "text": text
+    })
+
+
+def notify(subject, message):
+    if EMAIL_CONFIG["enabled"]:
+        send_email(subject, message)
+
+    if TELEGRAM_CONFIG["enabled"]:
+        send_telegram(f"{subject}\n\n{message}")
